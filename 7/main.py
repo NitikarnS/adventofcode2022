c879c91d977a8dce759003ed364b9ea3421334df
NitikarnS <nitikarnsna@gmail.com>
2022-12-09 01:33:56 +0700

    remove unused code

--- 7/main.py
+++ 7/main.py
@@ -1,5 +1,4 @@
-import aoc.utility as util
-
+import adventofcode2022.aoc.utility as util
 
 class Item:
     def __init__(self, name, size, type, items):
@@ -20,49 +19,6 @@ def main():
     day = 7
     local = 0
     util.mainProcess(year, day, local, getAnsPart1, getAnsPart2)
-
-
-def getAnsPart1Old(data):
-    lines = data.split("\n")
-    lines = [item for item in lines if item != "$ ls"]
-    lines = [item for item in lines if item != "$ ls" and item != "$ cd .."]
-    print(lines)
-    mapper = {}
-    temp = {}
-    for line in lines[::-1]:
-        if line.startswith("$ cd"):
-            mapper[line.split()[-1]] = temp
-            temp = {}
-        elif line.startswith("dir"):
-            dir = line.split()[-1]
-            # temp.append([getTotalSize(mapper.get(dir)), dir, "dir", mapper.get(dir)])
-            if (mapper.get(dir) == None):
-                print(f"line = {line}")
-                print(f"mapper = {mapper.keys()}")
-                print(f"temp = {temp.keys()}")
-                print("===============")
-                break
-            temp[dir] = [getTotalSize(mapper.get(dir)), "dir", mapper.get(dir)]
-            mapper.pop(dir)
-        else:
-            # temp.append([int(line.split()[0]), line.split()[1], "file", None])
-            temp[line.split()[1]] = [int(line.split()[0]), "file", None]
-
-        print(f"line = {line}")
-        print(f"mapper = {mapper.keys()}")
-        print(f"temp = {temp.keys()}")
-        print("===============")
-
-    sum = 0
-    condition = 100000
-    for key, value in mapper.items():
-        print(f"{key} : {value}")
-        size = getTotalSize(value)
-        if size <= condition:
-            sum += size
-
-        sum += sumFolderSizeLessThanMaxSize(condition, value)
-    return sum
 
 
 def getTotalSize(files):
